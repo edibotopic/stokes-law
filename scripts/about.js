2a613b8d6813914bfac0ef4dcfe987882e05b713
edibotopic <edibotopic@gmail.com>
2022-12-06 18:32:54 +0000

    corrected touch instructions for equation reveal

--- scripts/about.js
+++ scripts/about.js
@@ -5,7 +5,7 @@ const aboutContent =
     <span class="close">&times;</span>
 <p>A <strong>dispersion</strong> consists of particles with <strong>diameter</strong> <i>D</i> suspended in a solvent with <strong>viscosity</strong> <i>&#956;</i>.</p>
 <p>Separation occurs if there is a difference between <strong>particle density</strong> (<i>&#961;<sub>p</sub></i>) and <strong>solvent density</strong> (<i>&#961;<sub>s</sub></i>).</p>
-<p>The separation <strong>velocity</strong> <i>v</i> can be defined using Stoke's Law (<i>hover</i> or <i>long-press</i> to reveal):</p>
+<p>The separation <strong>velocity</strong> <i>v</i> can be defined using Stoke's Law (<i>hover</i> or <i>touch</i> to reveal):</p>
 <div class="deblur" width="100%">
 <img style="margin-left:auto; margin-right:auto;display:block" src="img/equation_light.png">
 </div>
