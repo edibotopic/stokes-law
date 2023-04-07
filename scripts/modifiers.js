c10a59a6e5a6760e6bb1f2c1996f66d9dc4d2e5a
edibotopic <edibotopic@gmail.com>
2023-04-07 16:39:51 +0100

    reformatted equation

--- scripts/modifiers.js
+++ scripts/modifiers.js
@@ -14,7 +14,16 @@ modifiers = () => {
         mu = newVisc;
 
         //velocity along the vertical (dy) axis for each particle instance is determined by Stoke's law
-        ball.dy = ((((ball.r * 2) ** 2) * (rhoP - rhoS) * (g * newGforce)) / (emp * mu));
+        ball.dy = (
+            (
+            (((ball.r * 2)) //convert radius to diameter
+            ** 2) //square the diameter value
+            * (rhoP - rhoS) //calculate the density difference
+            * (g * newGforce) //determine the gravitational or centrifugal force
+            )
+            / (emp * mu) //divide by empirical constant times the viscosity
+            / (DAMPING) // apply a damping factor for ease-of-visualisation
+        )                
     }
     )
 }
