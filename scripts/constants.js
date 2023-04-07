c0284869f5bc5626dd7a85f5b5e32329426c13d7
edibotopic <edibotopic@gmail.com>
2023-04-07 16:35:07 +0100

    updated constant names and values

--- scripts/constants.js
+++ scripts/constants.js
@@ -3,11 +3,15 @@ const COLOUR_BG = 'rgba(101,104,220)';
 const COLOURS_PARTS = ['rgba(306,333,50,0.7)','rgba(206,233,50,0.8)', 'rgba(196,183,50,0.6)', 'rgba(356,213,50,0.9)'];
 
 // Default physical values for Stoke's Law (NOTE: arbitrary)
-const DEFAULT_RADIUS = 15;
-const DEFAULT_RHO_P = 1.05;
-const DEFAULT_RHO_S = 1.00;
-const DEFAULT_GFORCE = 1.00;
+const DEFAULT_RADIUS = 10;
+const DEFAULT_RHO_P = 1050;
+const DEFAULT_RHO_S = 950;
+const DEFAULT_GFORCE = 1;
+const DEFAULT_GRAVITY = 9.81;
 const DEFAULT_VISCOSITY = 10;
 
 // Empirical constant used in Stoke's Law
 const EMPIRICAL_CONSTANT = 18;
+
+// Value to slow down visual speed of particles
+const DAMPING = 50000; 
