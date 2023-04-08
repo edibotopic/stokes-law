efce0d5d358fa4708ab353d54ded52e0cd847af6
edibotopic <edibotopic@gmail.com>
2023-04-08 10:13:51 +0100

    changed classes and titles for better tooltips

--- scripts/interface.js
+++ scripts/interface.js
@@ -27,39 +27,39 @@ const stepMu = 1;
 const sliders =
     `<div id="sliders">
       <div class="items">
-        <div class="item">
-        <p class="headers" slider title="units: mm">Particle Radius</p>
-        <input title="Adjust particle radius" id="slide-radius" type="range" min=${minRad} max=${maxD} value=${valRad} step=${stepRad} oninput="this.nextElementSibling.value = this.value">
+        <div title="units: mm" class="item" tabindex="0">
+        <p class="headers" slider> Particle Radius</p>
+        <input id="slide-radius" type="range" min=${minRad} max=${maxD} value=${valRad} step=${stepRad} oninput="this.nextElementSibling.value = this.value">
         <output id="sliderData1">${valRad}</output>
      </div> 
 
-      <div class="item">
-        <p class="headers" title="units: kg/m&#x00B3">Particle Density</p>
-        <input title="Adjust particle density" id="slide-rhoP" type="range" min=${minRhoP} max=${maxRhoP} value=${valRhoP} step=${stepRhoP} class="slider" oninput="this.nextElementSibling.value = this.value">
+      <div title="units: kg/m&#x00B3" class="item" tabindex="0">
+        <p class="headers">Particle Density</p>
+        <input id="slide-rhoP" type="range" min=${minRhoP} max=${maxRhoP} value=${valRhoP} step=${stepRhoP} class="slider" oninput="this.nextElementSibling.value = this.value">
         <output id="sliderData2">${valRhoP}</output>
       </div> 
 
-      <div class="item">
-        <p class="headers" title="units: kg/m&#x00B3">Solvent Density</p>
-        <input title="Adjust solvent density" id="slide-rhoS" type="range" min=${minRhoS} max=${maxRhoS} value=${valRhoS} step=${stepRhoS} oninput="this.nextElementSibling.value = this.value">
+      <div title="units: kg/m&#x00B3" class="item" tabindex="0">
+        <p class="headers">Solvent Density</p>
+        <input id="slide-rhoS" type="range" min=${minRhoS} max=${maxRhoS} value=${valRhoS} step=${stepRhoS} oninput="this.nextElementSibling.value = this.value">
         <output id="sliderData3">${valRhoS}</output>
       </div> 
 
-      <div class="item">
-        <p class="headers" title="units: none">Centrifugal Force</p>
-        <input title="Apply centrifugal force" id="slide-Gforce" type="range" min=${minG} max=${maxG} value=${valG} step=${stepG} class="slider" oninput="this.nextElementSibling.value = this.value">
+      <div title="units: none" class="item" tabindex="0">
+        <p class="headers">Centrifugal Force</p>
+        <input id="slide-Gforce" type="range" min=${minG} max=${maxG} value=${valG} step=${stepG} class="slider" oninput="this.nextElementSibling.value = this.value">
         <output id="sliderData4">${valG}</output>
       </div>
 
-      <div class="item">
-        <p class="headers" title="units: Pa.s">Solvent Viscosity</p>
-        <input title="Adjust solvent viscosity" id="slide-visc" type="range" min=${minMu} max=${maxMu} value=${valMu} step=${stepMu} oninput="this.nextElementSibling.value = this.value">
+      <div title="units: Pa.s" class="item" tabindex="0">
+        <p class="headers">Solvent Viscosity</p>
+        <input id="slide-visc" type="range" min=${minMu} max=${maxMu} value=${valMu} step=${stepMu} oninput="this.nextElementSibling.value = this.value">
         <output id="sliderData5">${valMu}</output>
       </div> 
 
     <br>
 
-      <div class="item">
+      <div class="buttons">
         <button title="Reload particles" id="restart"><i class="fa fa-repeat" aria-hidden="true"></i></button>
         <button title="Show data" id="graph"><i class="fa fa-line-chart" aria-hidden="true"></i></button>
         <button title="About the simulation" id="about")><i class="fa fa-question" aria-hidden="true"></i></button>
